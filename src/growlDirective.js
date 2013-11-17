595d79a80866287eda0f8df0c144eb4064f89535
Nathan Walker <nathan.walker@bizbuilt.com>
2013-11-17 13:40:05 -0800

    fix(deleteMessage): moving click handler up a level so it works for our use case. Clicking on the message should delete it. Also bumping version.

--- src/growlDirective.js
+++ src/growlDirective.js
@@ -10,7 +10,7 @@ angular.module('angular-growl').directive('growl', ['$rootScope', function ($roo
       //     '</div>',
 
       // BB Modified template
-      template: '<div class="growl">' + '\t<div class="contents alert" ng-repeat="message in messages" ng-class="computeClasses(message)">' + '\t\t<button type="button" class="close" ng-click="deleteMessage(message)">&times;</button>' + '<span class="title">{{message.text}}</span>' + '\t</div>' + '</div>',
+      template: '<div class="growl">' + '\t<div class="contents alert" ng-repeat="message in messages" ng-click="deleteMessage(message)" ng-class="computeClasses(message)">' + '\t\t<button type="button" class="close">&times;</button>' + '<span class="title">{{message.text}}</span>' + '\t</div>' + '</div>',
       // end modification
 
       replace: true,
@@ -20,7 +20,7 @@ angular.module('angular-growl').directive('growl', ['$rootScope', function ($roo
         '$timeout',
         function ($scope, $timeout) {
           $scope.messages = [];
-          
+
           // BB Modification
           $rootScope.$on('growlMessage', function (event, message) {
             if(message.replace){
